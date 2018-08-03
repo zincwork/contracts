58f9a8a20eb421d174ad89cd1098b4d1b51cb655
Dimitar <dimitar@rblock.co>
2018-08-03 12:20:45 +0100

    Move from addresse to hashes

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -6,18 +6,18 @@ contract Identity {
     uint256 constant READ_WRITE = 2;
     uint256 constant READ_ONLY = 1;
 
-    event AccessorAdded(address indexed key, uint256 indexed purpose);
-    event AccessorRemoved(address indexed key, uint256 indexed purpose);
+    event AccessorAdded(bytes32 indexed addressHash, uint256 indexed purpose);
+    event AccessorRemoved(bytes32 indexed addressHash, uint256 indexed purpose);
 
-    mapping(address => uint256) accessorMap;
+    mapping(bytes32 => uint256) accessorMap;
 
-    constructor(address initialAccessor) public {
+    constructor(bytes32 initialAccessor) public {
         accessorMap[initialAccessor] = MANAGEMENT;
-        emit AccessorAdded(msg.sender, MANAGEMENT);
+        emit AccessorAdded(initialAccessor, MANAGEMENT);
     }
 
     modifier allowedByPurpose(uint256 purpose) {
-        require(accessorMap[msg.sender] >= purpose, "Not authorized");
+        require(accessorMap[keccak256(abi.encode(msg.sender))] >= purpose, "Not authorized");
         _;
     }
 
@@ -26,22 +26,22 @@ contract Identity {
         _;
     }
 
-    function getAccessorPurpose(address key) public view returns(uint256) {
-        return accessorMap[key];
+    function getAccessorPurpose(bytes32 addressHash) public view returns(uint256) {
+        return accessorMap[addressHash];
     }
 
-    function addAccessor(address key, uint256 purpose) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose) {
-        uint256 oldPurpose = accessorMap[key];
+    function addAccessor(bytes32 addressHash, uint256 purpose) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose) {
+        uint256 oldPurpose = accessorMap[addressHash];
         if (oldPurpose > 0) {
-            emit AccessorRemoved(key, oldPurpose);
+            emit AccessorRemoved(addressHash, oldPurpose);
         }
-        accessorMap[key] = purpose;
-        emit AccessorAdded(key, purpose);
+        accessorMap[addressHash] = purpose;
+        emit AccessorAdded(addressHash, purpose);
     }
 
-    function removeAccessor(address key) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose)  {
-        uint256 purpose = accessorMap[key];
-        delete accessorMap[key];
-        emit AccessorRemoved(key, purpose);
+    function removeAccessor(bytes32 addressHash) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose)  {
+        uint256 purpose = accessorMap[addressHash];
+        delete accessorMap[addressHash];
+        emit AccessorRemoved(addressHash, purpose);
     }
 }
