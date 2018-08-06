39ca8cfa506a8df9c3473a5cf2830d88dc8040f7
Ali Azam <ali.azam@kcl.ac.uk>
2018-08-06 12:38:11 +0100

    indent SignatureValidator and update package.json to use yarn

--- contracts/SignatureValidator.sol
+++ contracts/SignatureValidator.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.4.24;
 
 contract SignatureValidator {
 
-	function doHash(string message1,uint32 message2, string header1, string header2) pure internal returns (bytes32) {
-	   return keccak256(
-	       abi.encodePacked(
-	       keccak256(abi.encodePacked(header1, header2)),
-	       keccak256(abi.encodePacked(message1, message2)))
-    );
-	}
+    function doHash(string message1, uint32 message2, string header1, string header2)
+     pure internal returns (bytes32) {
+        return keccak256(
+            abi.encodePacked(
+	              keccak256(abi.encodePacked(header1, header2)),
+	              keccak256(abi.encodePacked(message1, message2)))
+        );
+    }
 
-	function checkSignature(string message1, uint32 nounce, string header1, string header2, bytes32 r, bytes32 s, uint8 v) public pure returns (address) {
-	  bytes32 hash = doHash(message1, nounce, header1, header2);
-      return ecrecover(hash, v, r, s);
-	}
+    function checkSignature(string message1, uint32 nounce, string header1, string header2, bytes32 r, bytes32 s, uint8 v)
+     public pure returns (address) {
+        bytes32 hash = doHash(message1, nounce, header1, header2);
+        return ecrecover(hash, v, r, s);
+    }
 
 }
