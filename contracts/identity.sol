34a84d2e2d85ff63db0d52cf1f5595328bd24254
Dimitar <dimitar@rblock.co>
2018-08-06 11:25:32 +0100

    Review comments

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -2,45 +2,47 @@ pragma solidity ^0.4.24;
 
 // TODO: make payable
 contract Identity {
-    uint256 constant MANAGEMENT = 3;
-    uint256 constant READ_WRITE = 2;
-    uint256 constant READ_ONLY = 1;
+    uint8 constant MANAGEMENT = 3;
+    uint8 constant READ_WRITE = 2;
+    uint8 constant READ_ONLY = 1;
 
-    event AccessorAdded(address indexed key, uint256 indexed purpose);
-    event AccessorRemoved(address indexed key, uint256 indexed purpose);
+    event AccessorAdded(address indexed key, uint8 indexed purpose);
+    event AccessorRemoved(address indexed key, uint8 indexed purpose);
+    event AccessorUpdated(address indexed key, uint8 indexed oldPurpose, uint8 indexed newPurpose);
 
-    mapping(address => uint256) accessorMap;
+    mapping(address => uint8) accessorMap;
 
     constructor(address initialAccessor) public {
         accessorMap[initialAccessor] = MANAGEMENT;
         emit AccessorAdded(msg.sender, MANAGEMENT);
     }
 
-    modifier allowedByPurpose(uint256 purpose) {
+    modifier allowedByPurpose(uint8 purpose) {
         require(accessorMap[msg.sender] >= purpose, "Not authorized");
         _;
     }
 
-    modifier checkPurpose(uint256 purpose) {
+    modifier checkPurpose(uint8 purpose) {
         require(purpose >= READ_ONLY && purpose <= MANAGEMENT, "Invalid purpose");
         _;
     }
 
-    function getAccessorPurpose(address key) public view returns(uint256) {
+    function getAccessorPurpose(address key) public view returns(uint8) {
         return accessorMap[key];
     }
 
-    function addAccessor(address key, uint256 purpose) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose) {
-        uint256 oldPurpose = accessorMap[key];
-        if (oldPurpose > 0) {
-            emit AccessorRemoved(key, oldPurpose);
-        }
+    function addAccessor(address key, uint8 purpose) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose) {
+        uint8 oldPurpose = accessorMap[key];
         accessorMap[key] = purpose;
-        emit AccessorAdded(key, purpose);
+        if (oldPurpose != 0) {
+            emit AccessorUpdated(key, oldPurpose, purpose);
+        } else {
+            emit AccessorAdded(key, purpose);
+        }
     }
 
     function removeAccessor(address key) public allowedByPurpose(MANAGEMENT) checkPurpose(purpose)  {
-        uint256 purpose = accessorMap[key];
+        uint8 purpose = accessorMap[key];
         delete accessorMap[key];
         emit AccessorRemoved(key, purpose);
     }
